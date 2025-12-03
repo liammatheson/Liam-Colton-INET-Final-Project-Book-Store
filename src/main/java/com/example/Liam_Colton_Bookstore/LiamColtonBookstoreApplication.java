dc58ac2b7536d38c35cbfd91deb5dec9234dec89
w0494713 <w0494713@nscc.ca>
2025-12-03 14:41:28 -0400

    added basic springboot project

---
+++ src/main/java/com/example/Liam_Colton_Bookstore/LiamColtonBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Liam_Colton_Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiamColtonBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiamColtonBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Liam_Colton_Bookstore/LiamColtonBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Liam_Colton_Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiamColtonBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
