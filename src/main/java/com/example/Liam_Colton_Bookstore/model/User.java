d0cb2a52306fda23225158b789d12f12aa4773e5
w0494713 <w0494713@nscc.ca>
2025-12-09 12:59:29 -0400

    improved general look. added cart button that expands and collapses and is set up with thymeleaf to show the cart once that stuff is finished. redid product-details page, picture and details are on the left. price and big yellow add to cart button are on the right.

--- src/main/java/com/example/Liam_Colton_Bookstore/model/User.java
+++ src/main/java/com/example/Liam_Colton_Bookstore/model/User.java
@@ -1,6 +1,11 @@
 package com.example.Liam_Colton_Bookstore.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -29,8 +34,6 @@ public class User {
     message = "Password must contain upper, lower, and a number"
     )
     private String password;
-
-
     
     public User() {}
 
